18c07de4b5fd9a87a503123b127be1c6b21140fe
jon albert virrey <113243693+Jonvry@users.noreply.github.com>
2023-11-17 18:10:30 +0800

    remove CredentialsSignin

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,4 @@
-'use client';
- 
+'use client'; 
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -67,7 +66,8 @@ export default function LoginForm() {
           aria-live="polite"
           aria-atomic="true"
         >
-          {state === 'CredentialsSignin' && (
+          {/* state === 'CredentialsSignin' &&  */}
+          {state && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">Invalid credentials</p>
